c57ef36e99a127620d99695c8ddc436dd493d06b
yhtham1 <yhtham1@m1.pxaa.net>
2021-04-09 17:01:30 +0900

    ftdiパートの追加

--- ftdi_reset.py
+++ ftdi_reset.py
@@ -2,27 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
+import configparser
+import serial.tools.list_ports
 import ft232
 import time
 
 
-def get_serialnumber(port):
-	print(port)
-	return port
+def get_serialnumber(target_port):
+	target_port = target_port.upper()
+	# print(target_port)
+	ports = list(serial.tools.list_ports.comports())
+	ans = None
+	for it in ports:
+		if it.name.upper() == target_port:
+			ans = it.serial_number[:-1]
+			print('found:{}-{}'.format(target_port, ans))
+		else:
+			print('{} {} {}'.format(it.name.upper(), it.serial_number, it.manufacturer))
+	return ans
+
 
 def main():
-
 	inifile = configparser.ConfigParser()
 	inifile.read('bootloader.ini')
 	comport = inifile.get('settings', 'comport')
 	serial_number = get_serialnumber(comport)
 	print(serial_number)
-	return
-
-
-
-	# serial_number = "DJ5LV1RQ"
-	serial_number = "DJ61E92G"
 	sp = []
 	try:
 		sp = ft232.Ft232(serial_number, baudrate=115200)
@@ -37,20 +43,22 @@ def main():
 	# If you want to use the CBUS pins, you enable them with cbus_setup
 	# 'mask' is a bitmask which specifies which pins to enable
 	# 'init' is a bitmask for the initial value for each pin
-	sp.cbus_setup(mask=14, init=0)
-
+	sp.cbus_setup(mask=15, init=1)
 	# Change the current value of all setup pins
-	sp.cbus_write(7)
+	sp.cbus_write(0)
+	time.sleep(3)
+
+	sp.cbus_setup(mask=14, init=0)
 	return
-	while True:
-		sp.cbus_write(1)
-		time.sleep(0.1)
-		sp.cbus_write(2)
-		time.sleep(0.1)
-		sp.cbus_write(4)
-		time.sleep(0.1)
-		sp.cbus_write(8)
-		time.sleep(0.1)
+	# while True:
+	# 	sp.cbus_write(1)
+	# 	time.sleep(0.1)
+	# 	sp.cbus_write(2)
+	# 	time.sleep(0.1)
+	# 	sp.cbus_write(4)
+	# 	time.sleep(0.1)
+	# 	sp.cbus_write(8)
+	# 	time.sleep(0.1)
 
 	# Print the current value of all setup pins
 	print("CBUS: %s" % sp.cbus_read())
