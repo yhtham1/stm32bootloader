22d95ddf6a371a0bc38af7036f72fe57b1eaf6db
yhtham1 <yhtham1@m1.pxaa.net>
2021-08-25 18:02:34 +0900

    ftdiパートの追加

--- ftdi_reset.py
+++ ftdi_reset.py
@@ -8,6 +8,7 @@ import serial.tools.list_ports
 import ft232
 import time
 
+
 def port_detail(p):
 	print('----------------------------------------------------------')
 	print('device       :{}'.format(p.device))
@@ -20,6 +21,7 @@ def port_detail(p):
 	print('product      :{}'.format(p.product))
 	print('interface    :{}'.format(p.interface))
 
+
 def get_serialnumber(target_port):
 	target_port = target_port.upper()
 	print('target={}'.format(target_port))
@@ -27,7 +29,7 @@ def get_serialnumber(target_port):
 	ans = None
 	for it in ports:
 		# port_detail(it)
-		print('---------------------',it.name)
+		print('---------------------', it.name)
 		if it.name.upper() == target_port:
 			if None != it.serial_number:
 				ans = it.serial_number[:-1]
@@ -39,11 +41,43 @@ def get_serialnumber(target_port):
 	return ans
 
 
+# cusb bit0: NRST
+# cusb bit1: BOOT0
+# cusb bit2: BOOT1
+# cusb bit3: PB14
+
+# n:0 NORMAL BOOT
+# n:1 BOOT LOADER
+# n:2 not use
+# n:3 RAM BOOT
+
+def reboot(sp, n):
+	pat = int(n * 2)
+	sp.cbus_setup(mask=0x0f, init=pat)
+	sp.cbus_write(pat)
+	time.sleep(0.1)
+	sp.cbus_setup(mask=0x0e, init=pat)
+
+
 def main():
 	inifile = configparser.ConfigParser()
 	inifile.read('bootloader.ini')
 	comport = inifile.get('settings', 'comport')
 	serial_number = get_serialnumber(comport)
+
+	i = 1
+	boot_mode = 3
+	while i < len(sys.argv):  # in range(1, len(sys.argv)):
+		arg2 = sys.argv[i].strip()
+		if 0 <= arg2.find('--port'):
+			comport = sys.argv[i + 1].strip()
+			i += 2
+			continue
+		if 0 <= arg2.find('--mode'):
+			boot_mode = int(sys.argv[i + 1].strip())
+			continue
+		i += 1
+
 	if None == serial_number:
 		print('serial_number not found')
 		return
@@ -55,6 +89,10 @@ def main():
 		print("Unable to open the ftdi device: %s" % serial_number)
 		sys.exit(1)
 
+	reboot(sp, boot_mode)
+
+
+
 	# You may use sp as you would a Serial object
 	# sp.write(b"Hello World!\n")
 	# resp = sp.read(100)
@@ -62,13 +100,6 @@ def main():
 	# If you want to use the CBUS pins, you enable them with cbus_setup
 	# 'mask' is a bitmask which specifies which pins to enable
 	# 'init' is a bitmask for the initial value for each pin
-	sp.cbus_setup(mask=15, init=1)
-	# Change the current value of all setup pins
-	sp.cbus_write(0)
-	time.sleep(3)
-
-	sp.cbus_setup(mask=14, init=0)
-	return
 	# while True:
 	# 	sp.cbus_write(1)
 	# 	time.sleep(0.1)
